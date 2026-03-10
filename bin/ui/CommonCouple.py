b5fa6113b40a34aa04a2a6b52fe97047bd154b55
YChenY5260 <chinggottx322@outlook.com>
2026-03-10 19:42:18 +0800

    添加了登录界面, 未完成注册界面及界面切换

---
+++ bin/ui/CommonCouple.py
@@ -0,0 +1,215 @@
+# -*- coding: utf-8 -*-
+
+'''
+一些常见的Qt组件的组合
+提取出来方便代码复用
+'''
+
+from typing import Tuple
+from PySide6.QtWidgets import (QLayout, QHBoxLayout, QVBoxLayout, QWidget, QPushButton, QLabel, QSizePolicy, QLineEdit)
+from PySide6.QtGui import QFont
+from PySide6.QtCore import Qt
+
+class Fonts:
+    '''
+    常用跨平台字体家族
+    '''
+    
+    UniversalPlainFont = QFont("Segoe UI, Arial, Liberation Sans, DejaVu Sans, sans-serif", 16)             # 用于界面显示
+    MonospacedCodeFont = QFont("Consolas, Courier New, Liberation Mono, DejaVu Sans Mono, monospace", 16)   # 用于代码编写
+    SerifPrintFont = QFont("Times New Roman, Liberation Serif, DejaVu Serif, serif", 16)                    # 用于打印阅读
+
+    @classmethod
+    def sizedFont(cls, font: QFont, size: int):
+        '''
+        返回修改过大小的字体
+
+        Args:
+            font(QFont): 字体
+            size(int): 字体大小
+        
+        Returns:
+            QFont: 字体类
+        
+        Raises:
+            None
+        '''
+
+        font.setPointSize(size)
+        return font
+
+
+class TextInput(QHBoxLayout):
+    '''
+    横向输入框, 由描述和输入框组成
+    '''
+
+    Hidden = 0
+    Displayed = 1
+
+    def __init__(self, text: str, phText: str = '', mode: int = 1, size: Tuple[int, int] = (400, 40) , font: QFont = Fonts.UniversalPlainFont):
+        '''
+        创建QLabel和QLineEdit组件并应用至布局
+
+        Args:
+            text(str): 标签内容
+            phText(str): 占位符文本内容
+            mode(int): 输入框显示模式, Hidden是隐藏, Displayed是显示
+            size(Tuple[int, int]): 文本输入框尺寸
+            font(QFont): 标签及文本输入框的字体
+        
+        Returns:
+            None: 该函数没有返回值
+        
+        Raises:
+            参数mode非 0, 1 时抛出ValueError错误
+        '''
+        super().__init__()
+
+        # 创建并设置标签组件
+        qText = QLabel(text)
+        qText.setFont(font)
+        qText.setSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
+
+        # 创建并设置文本输入框组件
+        qInput = QLineEdit()
+        qInput.setFont(font)
+        qInput.setPlaceholderText(phText)
+        qInput.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        qInput.setFixedSize(size[0], size[1])
+
+        if mode == self.Hidden:
+            qInput.setEchoMode(QLineEdit.EchoMode.Password)
+        elif mode == self.Displayed:
+            qInput.setEchoMode(QLineEdit.EchoMode.Normal)
+        else:
+            raise ValueError("参数mode超出范围")
+
+        # 定义获取输入值的函数
+        self.getInput = lambda: qInput.text()
+
+        # 设置布局属性
+        self.setSizeConstraint(QLayout.SizeConstraint.SetMinimumSize)
+        self.setContentsMargins(0, 0, 0, 0)
+        self.setSpacing(0)
+
+        # 添加组件
+        self.addWidget(qText, 0, alignment=Qt.AlignmentFlag.AlignLeft)
+        self.addWidget(qInput, 0, alignment=Qt.AlignmentFlag.AlignLeft)
+
+class Button(QPushButton):
+    '''
+    按钮
+    '''
+
+    def __init__(self, text: str, tip: str = '', size: Tuple[int, int] = (200, 40), font: QFont = Fonts.UniversalPlainFont):
+        '''
+        设置按钮组件的属性
+
+        Args:
+            text(str): 按钮上显示的文本
+            tip(str): 按钮工具提示, 悬浮文本
+            size(Tuple[int, int]): 按钮的尺寸
+            font(QFont): 按钮上文本的字体
+        
+        Returns:
+            None
+        
+        Raises:
+            None
+        '''
+        super().__init__()
+
+        self.setFont(Fonts.UniversalPlainFont)
+        self.setText(text)
+        self.setToolTip(tip)
+        self.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        self.setFixedSize(size[0], size[1])
+
+class ClassicLayout():
+    '''
+    经典布局
+    '''
+
+    # 常用对其方式
+    Left = Qt.AlignmentFlag.AlignLeft
+    Right = Qt.AlignmentFlag.AlignRight
+    Top = Qt.AlignmentFlag.AlignTop
+    Bottom = Qt.AlignmentFlag.AlignBottom
+    Center = Qt.AlignmentFlag.AlignCenter
+    HCenter = Qt.AlignmentFlag.AlignHCenter
+    VCenter = Qt.AlignmentFlag.AlignVCenter
+    CTop = Top | HCenter
+    CBottom = Bottom | HCenter
+    CLeft = Left | VCenter
+    CRight = Right | VCenter
+
+    # 常用约束
+    Default = QLayout.SizeConstraint.SetDefaultConstraint
+    Max = QLayout.SizeConstraint.SetMaximumSize
+    Min = QLayout.SizeConstraint.SetMinimumSize
+    MinMax = QLayout.SizeConstraint.SetMinAndMaxSize
+    No = QLayout.SizeConstraint.SetNoConstraint
+    Fixed = QLayout.SizeConstraint.SetFixedSize
+
+    # 常用边界间距
+    NoBorder = (0, 0, 0, 0)
+    SmallBorder = (20, 20, 20, 20)
+    LargeBorder = (40, 40, 40, 40)
+
+    class Horizontal(QHBoxLayout):
+        '''
+        水平布局
+        '''
+
+        def __init__(self, align: Qt.AlignmentFlag = Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignLeft, constr: QLayout.SizeConstraint = QLayout.SizeConstraint.SetDefaultConstraint, margins: Tuple[int, int, int, int] = (0, 0, 0, 0), spacing: int = 0):
+            '''
+            设置常用的布局属性
+
+            Args:
+                align(AlignmentFlag): 对其方向
+                constr(SizeConstraint): 尺寸约束
+                margins(Tuple[int, int, int, int]): 组件距边界距离
+                spacing(int): 内部组件间距离
+            
+            Returns:
+                None
+
+            Raises:
+                None
+            '''
+            super().__init__()
+
+            self.setAlignment(align)
+            self.setSizeConstraint(constr)
+            self.setContentsMargins(margins[0], margins[1], margins[2], margins[3])
+            self.setSpacing(spacing)
+
+    class Vertical(QVBoxLayout):
+        '''
+        垂直布局
+        '''
+
+        def __init__(self, align: Qt.AlignmentFlag = Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignLeft, constr: QLayout.SizeConstraint = QLayout.SizeConstraint.SetDefaultConstraint, margins: Tuple[int, int, int, int] = (0, 0, 0, 0), spacing: int = 0):
+            '''
+            设置常用的布局属性
+
+            Args:
+                align(AlignmentFlag): 对其方向
+                constr(SizeConstraint): 尺寸约束
+                margins(Tuple[int, int, int, int]): 组件距边界距离
+                spacing(int): 内部组件间距离
+            
+            Returns:
+                None
+
+            Raises:
+                None
+            '''
+            super().__init__()
+
+            self.setAlignment(align)
+            self.setSizeConstraint(constr)
+            self.setContentsMargins(margins[0], margins[1], margins[2], margins[3])
+            self.setSpacing(spacing)
+
--- bin/ui/Login.py
+++ bin/ui/Login.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 
 import os
+import sys
 from pathlib import Path
+from PySide6.QtWidgets import (QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QLayout, QSizePolicy, QLineEdit,
+                               QStackedLayout)
+from PySide6.QtCore import Qt
 
-from PySide6.QtWidgets import (QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QLayout, QSizePolicy, QLineEdit)
+from CommonCouple import TextInput, Button, ClassicLayout, Fonts
 
 class LoginWindow(QWidget):
     '''
-    * 外观
     登录窗口, 继承自QWidget
+
+    * 外观
     有登录和注册两种界面
     登录界面包含UID或昵称输入框和密码输入框
     注册界面包含昵称输入框、密码输入框和确认密码输入框
@@ -24,4 +29,120 @@ class LoginWindow(QWidget):
     5. 若要注册账户, 注册成功则提示并切换到登录界面, 注册失败仍要提示
     6. 若要登录账户, 登录成功则进入主界面, 否则提示错误
     '''
+
+    def __init__(self):
+        super().__init__()
+
+        # 窗口属性设置
+        self.setWindowTitle("登录")                # 窗口标题
+        self.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        self.setFixedSize(600, 300)               # 窗口尺寸
+
+        self.initUI()
+
+    def initUI(self):
+        self.creWidgets()
+        self.applyLayout()
+
+    def creWidgets(self):
+        '''
+        仅创建所有用到的组件, 不设置布局
+        '''
+
+        # UID或昵称(以下用ID代替)输入框
+        self.idInputer = TextInput("账户: ", "请输入你的UID或昵称")
+        self.idInputer.setAlignment(ClassicLayout.Left)
+        # 密码输入框
+        self.pwdInputer = TextInput("密码: ", "请输入密码", TextInput.Hidden)
+        self.pwdInputer.setAlignment(ClassicLayout.Left)
+        # 密码确认框
+        self.pwdVerification = TextInput("确认密码: ", "再次输入相同的密码", TextInput.Hidden)
+        self.pwdVerification.setAlignment(ClassicLayout.Left)
+
+        # 大登录按钮
+        self.loginButton = Button("登录", "信息填写完毕后, 点击该按钮即可登录")
+        # 大注册按钮
+        self.registerButton = Button("注册", "信息填写完毕后, 点击该按钮即可注册")
+        # 小找回密码按钮-登录界面
+        self.pwdFoundButtonLogin = Button("找回密码", size=(100, 24), font=Fonts.sizedFont(Fonts.UniversalPlainFont, 10))
+        # 小找回密码按钮-注册界面
+        self.pwdFoundButtonRegister = Button("找回密码", size=(100, 24), font=Fonts.sizedFont(Fonts.UniversalPlainFont, 10))
+        # 小注册账户按钮
+        self.regAccountButton = Button("注册账户", size=(100, 24), font=Fonts.sizedFont(Fonts.UniversalPlainFont, 10))
+        # 小登录账户按钮
+        self.loginAccountButton = Button("登录账户", size=(100, 24), font=Fonts.sizedFont(Fonts.UniversalPlainFont, 10))
+
+        # 登录界面
+        self.loginWindow = QWidget()
+        self.loginWindow.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        self.loginWindow.setFixedSize(600, 300)
+
+        # 注册界面
+        self.registerWindow = QWidget()
+        self.registerWindow.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        self.registerWindow.setFixedSize(600, 300)
+
+    def applyLayout(self):
+        '''
+        添加并设置布局
+        '''
+
+        # 主布局, 垂直布局
+        self.mainLayout = ClassicLayout.Vertical(constr=ClassicLayout.Default, margins=ClassicLayout.NoBorder, spacing=10)
+
+        # 找回密码与注册账户布局
+        self.pnrLayout = ClassicLayout.Horizontal(constr=ClassicLayout.MinMax, margins=(80, 0, 80, 0), spacing=0)
+
+        self.pnrLayout.addWidget(self.pwdFoundButtonLogin, 0, alignment=Qt.AlignmentFlag.AlignLeft)
+        self.pnrLayout.addStretch(1)
+        self.pnrLayout.addWidget(self.regAccountButton, 0, alignment=Qt.AlignmentFlag.AlignRight)
+
+        # 找回密码与登录账户布局
+        self.pnlLayout = ClassicLayout.Horizontal(constr=ClassicLayout.MinMax, margins=(80, 0, 80, 0), spacing=0)
+
+        self.pnlLayout.addWidget(self.pwdFoundButtonRegister, 0, alignment=Qt.AlignmentFlag.AlignLeft)
+        self.pnlLayout.addStretch(1)
+        self.pnlLayout.addWidget(self.loginAccountButton, 0, alignment=Qt.AlignmentFlag.AlignRight)
+
+        # 登录界面布局
+        self.loginLayout = ClassicLayout.Vertical(align=ClassicLayout.CTop, constr=ClassicLayout.MinMax, margins=(80, 40, 80, 40), spacing=10)
+
+        self.loginLayout.addLayout(self.idInputer, 0)
+        self.loginLayout.addLayout(self.pwdInputer, 0)
+        self.loginLayout.addLayout(self.pnrLayout, 0)
+        self.loginLayout.addStretch(1)
+        self.loginLayout.addWidget(self.loginButton, 0, alignment=ClassicLayout.CBottom)
+
+        # 注册界面布局
+        self.registerLayout = ClassicLayout.Vertical(align=ClassicLayout.CTop, constr=ClassicLayout.MinMax, margins=(80, 40, 80, 40), spacing=10)
+
+        self.registerLayout.addLayout(self.idInputer, 0)
+        self.registerLayout.addLayout(self.pwdInputer, 0)
+        self.registerLayout.addLayout(self.pwdVerification, 0)
+        self.registerLayout.addLayout(self.pnlLayout, 0)
+        self.registerLayout.addStretch(1)
+        self.registerLayout.addWidget(self.registerButton, 0, alignment=ClassicLayout.CBottom)
+
+        # 切换布局, 用于切换登录界面和注册界面
+        self.switchLayout = QStackedLayout()
+        self.switchLayout.setSizeConstraint(ClassicLayout.MinMax)
+        self.switchLayout.setContentsMargins(0, 0, 0, 0)
+        self.switchLayout.setSpacing(0)
+        
+        self.loginWindow.setLayout(self.loginLayout)
+        self.registerWindow.setLayout(self.registerLayout)
+
+        self.switchLayout.addWidget(self.loginWindow)
+        self.switchLayout.addWidget(self.registerWindow)
+
+        # 应用至主布局
+        self.mainLayout.addLayout(self.switchLayout, 1)
+
+        self.setLayout(self.mainLayout)
+
+if __name__ == '__main__':
+    MMApp = QApplication(sys.argv)
+    Window = LoginWindow()
+    Window.show()
+    MMApp.exec()
 
